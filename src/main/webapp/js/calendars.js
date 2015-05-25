0b858712db9cc3539f3f06f74c4000c32a595a1d
Alicia Tang <alicia.x.tang@gmail.com>
2015-05-25 15:15:42 -0700

    toggle calendar

--- src/main/webapp/js/calendars.js
+++ src/main/webapp/js/calendars.js
@@ -1,21 +1,27 @@
+
 $(document).ready(function() {
-	console.log("calendar.js");
 	$.get('/calendar', null, function(data){
-		console.log(data);
 		var map = {};
 		var list = [];
 		$.each(data, function(index, cal){
 			map[cal.id] = cal;
 			list.push("<li id='"+cal.id+"' style='background-color:"+ cal.color+"'>" + cal.name + "</li>");
+			$('#calendar').fullCalendar( 'addEventSource', cal);
 		});
 		$("#gcalendar").append(list);
 		$("#gcalendar li").click(function(){
 			$(this).toggleClass("disabled");
 			var id = $(this).attr('id');
-			var bg = map[id]["color"];
-			$(this).css('background-color', $(this).hasClass("disabled") ? "" : bg);
+			var source = map[id];
+			if ($(this).hasClass("disabled")) {				
+				$(this).css('background-color',  "");
+				$('#calendar').fullCalendar('removeEventSource', source);
+			} else {
+				$(this).css('background-color',  source["color"]);
+				$('#calendar').fullCalendar('addEventSource', source);
+			}
 		});
-		
-		$('#calendar').fullCalendar({eventSources: data});
 	}, 'json');
+	$('#calendar').fullCalendar();
 });
+
